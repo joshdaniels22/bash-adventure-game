3f85e4c068f135cc4543e2e61a48c1cf0e2651c8
Joshua Daniels <joshuadaniels537@gmail.com>
2023-04-04 23:35:38 +0200

    feature: entry to game

---
+++ script.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Welcome to the Enchated Forest. You will have to manouvre through it to the the top of the mountain.\n\nWhich route would you like to take?\n\n 1) The Forbidden Swamp\n 2) The Wolf Lands\n 3) The Dwarf Fields\n\n"
+
+read -r answer
+
+echo $answer
